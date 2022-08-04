892582b585fdb8bfabfb7249ed10e15cc6012942
Mayur Sonowal <51331982+hellraiser7@users.noreply.github.com>
2022-08-04 22:20:10 +0530

    update readme 11th program

---
+++ Blind 75/climbing_stairs_70.py
@@ -0,0 +1,21 @@
+# DP now
+def climbStairsDP(n):
+    # Calculate the recursion relation first
+    # Do it for 2 steps, 3 steps, then 4. There will be a pattern emerging
+    # climbStairs(n) = climbStairs(n-1) + climbStairs(n-2)
+    if (n <= 3):
+        return n
+    T = [0]*(n+1)
+    
+    T[2] = 2
+    T[3] = 3
+
+    #bottom-up
+    for i in range(4,n+1):
+        T[i] = T[i-1] + T[i-2]
+    return T[n]
+
+if __name__ == "__main__":
+    n = 6
+    print(climbStairsDP(n))
+
