63db3d32568b6288ace52761212271829821af5e
Mayur Sonowal <51331982+hellraiser7@users.noreply.github.com>
2022-08-09 03:33:55 +0530

    add optimized solution

--- Blind 75/group_anagrams_49.py
+++ Blind 75/group_anagrams_49.py
@@ -22,6 +22,16 @@ def groupAnagrams(strs):
         innerElements = []
     return result
 
+def groupAnagramsOptimized(strs):
+    # use of a hashmap and some ascii shit
+    frequency = defaultdict(list)
+    for s in strs:
+        countArray = [0]*26 # for all 16 alphabet counting
+        for char in s:
+            # make the frequency chart
+            countArray[ord(s) - ord('a')] += 1
+        frequency[tuple(countArray)].append(s) # if abc, then countArray = [1,1,1,0,0,0....0] till 26 
+    return frequency.values() # same value tuples will get clubbed into one value array. Return all those strings
 if __name__ == "__main__":
     strs = ["end","ned", "gone", "nope", "hope","succulent", "den", "pone","luccusent"]
     print("Anagrams grouping: ", groupAnagrams(strs))
