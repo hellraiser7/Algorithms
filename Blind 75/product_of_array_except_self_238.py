0ac891bef54008e4d46662c90e5b511f8bd52c4b
Mayur Sonowal <51331982+hellraiser7@users.noreply.github.com>
2022-08-15 23:28:36 +0530

    optimized O(n) version

--- Blind 75/product_of_array_except_self_238.py
+++ Blind 75/product_of_array_except_self_238.py
@@ -12,7 +12,21 @@ def getProduct(nums):
         product = product * i
     return product
 
+def productExceptSelf(nums):
+        res = [1] * (len(nums))
+
+        prefix = 1
+        for i in range(len(nums)):
+            res[i] = prefix
+            prefix *= nums[i]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
 if __name__ == "__main__":
     nums = [1,2,3,4]
     print("Product except self: ", product_except_self(nums))
+    print("Optimized way: ", productExceptSelf(nums))
 
