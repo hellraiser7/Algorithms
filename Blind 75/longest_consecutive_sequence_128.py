e56e4598881ebb14e3cc257c05e9f9fcb18e87c9
Mayur Sonowal <51331982+hellraiser7@users.noreply.github.com>
2022-08-18 04:18:07 +0530

    Update longest_consecutive_sequence_128.py

--- Blind 75/longest_consecutive_sequence_128.py
+++ Blind 75/longest_consecutive_sequence_128.py
@@ -22,9 +22,24 @@ def longestConsecutive_Sort(nums):
     # hence we need to return maximum of those two in order to eliminate any ambiguity
     return max(longestStreak, currentStreak)
 
+# Time: O(n) | Space: O(n)
+def longestConsecutiveOptimized(nums):
+    # use numsSet, but check the starting element for the sequence
+    # iterate, and if the element does not have a number at the left which is one lesser than itself, it is a starting element
+    # then iterate from this starting element, keep checking if next consecutive element exists or not
+    # if it does, then keep incrementing local length
+    numsSet = set(nums)
+    longestStreak = 0
+    for i in range(len(nums)):
+        # check if starting ele
+        if (nums[i]-1) not in numsSet:
+            currentStreak = 0
+            while (nums[i] + currentStreak) in numsSet:
+                currentStreak += 1
+            longestStreak = max(longestStreak, currentStreak)
+
+    return longestStreak
 if __name__ == "__main__":
     nums = [100,200,4,300,7,6,5]
     print("longest consecutive sequence: ", longestConsecutive_Sort(nums))
-    
-        
-
+    print("longest consecutive sequence with optimization: ", longestConsecutiveOptimized(nums))
